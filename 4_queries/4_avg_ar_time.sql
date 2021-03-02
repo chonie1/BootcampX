c3f54b02d9f8bd13d5bea969b8451f394f2bf1fc
Connie Ho <connie3@ualberta.ca>
2021-03-02 00:24:01 +0000

    add 4 queries

---
+++ 4_queries/4_avg_ar_time.sql
@@ -0,0 +1,2 @@
+SELECT AVG(completed_at - started_at) AS avg_ar_duration
+FROM assistance_requests;
---
+++ 4_queries/6_long_cohort_time.sql
@@ -0,0 +1,16 @@
+SELECT cohorts.name AS name, AVG(completed_at - started_at) AS avg_assistance_time
+FROM assistance_requests
+JOIN students ON students.id = student_id
+JOIN cohorts ON cohorts.id = cohort_id
+GROUP BY cohorts.name
+ORDER BY avg_assistance_time DESC
+LIMIT 1;
+
+
+-- SELECT cohorts.name, avg(completed_at - started_at) as average_assistance_time
+-- FROM assistance_requests 
+-- JOIN students ON students.id = assistance_requests.student_id
+-- JOIN cohorts ON cohorts.id = cohort_id
+-- GROUP BY cohorts.name
+-- ORDER BY average_assistance_time DESC
+-- LIMIT 1;
